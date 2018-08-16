21fbfcd2955820b16eb793d92f002f3ea52956d1
Tuna Hsu <tunahsu45@gmail.com>
2018-08-16 20:33:20 +0800

    Fixed Position.

--- app/routes.py
+++ app/routes.py
@@ -5,7 +5,7 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    names = sorted(os.listdir('C:/Users/winproluhao/Desktop/c8763/app/static/img/outcome/name/'))
-    tasks = sorted(os.listdir('C:/Users/winproluhao/Desktop/c8763/app/static/img/outcome/task/'))
+    names = sorted(os.listdir('C:/Users/tunah/Desktop/c8763/app/static/img/outcome/name/'))
+    tasks = sorted(os.listdir('C:/Users/tunah/Desktop/c8763/app/static/img/outcome/task/'))
     
     return render_template('show.html', names=names, tasks=tasks)
