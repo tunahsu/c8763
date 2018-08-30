cd404e8cb9b35d62d9ee89c76a9d9c17da64e833
Tuna Hsu <tunahsu45@gmail.com>
2018-08-30 23:28:44 +0800

    Add uploader.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 import os
+import json
+import glob
+from datetime import datetime
 from app import app
 
 @app.route('/')
@@ -17,3 +20,57 @@ def index():
     tasks = sorted(os.listdir(path2))
     
     return render_template('show.html', names=names, tasks=tasks)
+
+@app.route("/new")
+def new():
+    return render_template("new.html")
+
+@app.route("/upload", methods=["POST"])
+def upload():
+    """Handle the upload of a file."""
+    form = request.form
+
+    # Create a unique "session ID" for this particular batch of uploads.
+    upload_key = datetime.strftime(datetime.now(), '%Y-%m-%d')
+
+    # Is the upload using Ajax, or a direct POST by the form?
+    is_ajax = False
+    if form.get("__ajax", None) == "true":
+        is_ajax = True
+
+    # Target folder for these uploads.
+    if form.get("imgtype", None) == "name":
+        target = "app/static/img/name/{}".format(upload_key)
+    elif form.get("imgtype", None) == "task":
+        target = "app/static/img/task/{}".format(upload_key)
+    try:
+        os.mkdir(target)
+    except:
+        if is_ajax:
+            return ajax_response(False, "Couldn't create upload directory: {}".format(target))
+        else:
+            return "Couldn't create upload directory: {}".format(target)
+
+    print("=== Form Data ===")
+    for key, value in list(form.items()):
+        print(key, "=>", value)
+
+    for upload in request.files.getlist("file"):
+        filename = upload.filename.rsplit("/")[0]
+        destination = "/".join([target, filename])
+        print("Accept incoming file:", filename)
+        print("Save it to:", destination)
+        upload.save(destination)
+
+    if is_ajax:
+        return ajax_response(True, upload_key)
+    else:
+        return redirect(url_for("upload_complete", uuid=upload_key))
+
+
+def ajax_response(status, msg):
+    status_code = "ok" if status else "error"
+    return json.dumps(dict(
+        status=status_code,
+        msg=msg,
+    ))
